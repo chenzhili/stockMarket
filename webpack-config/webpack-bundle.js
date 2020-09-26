c36ef7b4d6a63e731e77bd85821c2cddf5fa838c
chenzhili <1845882620@qq.com>
2020-09-26 17:18:53 +0800

    解决打包出现eval

--- webpack-config/webpack-bundle.js
+++ webpack-config/webpack-bundle.js
@@ -64,7 +64,14 @@ const webpackConfig = merge(commonConfig, {
   ],
   resolve: {
     aliasFields: ['browser']
-  }
+  },
+  target: 'web',
+  /* 这句配置就能让 eval 函数消失 */
+  /* 
+    https://github.com/webpack/webpack/issues/5627 
+    Edit: Using webpack 4.1.1 with an explicit override of devtool: 'inline-source-map' to disable the default eval source-map for the development environment, I'm successfully able to produce an eval-less output for both production and development environments.
+  */
+  devtool: false
 })
 
 webpackConfig.mode = process.env.MINIFY && process.env.MINIFY === 'true' ? 'production' : 'development'
